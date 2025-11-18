f5ab331e4983f13211e4ebe1a54489b67d627501
jiy0ungkim <2626kjy@naver.com>
2025-11-18 15:40:22 +0900

    02

--- main.c
+++ main.c
@@ -1,20 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
-
+#include "board.h"
 
 int main(int argc, char *argv[])
 {
     srand((unsigned)time(NULL));
     
-    // opening
+    //opening
     printf("==================================================\n");
     printf("                 SHARK GAME START                 \n");
     printf("==================================================\n");
     
+    //step 1. Initialization (player name setting + variables)
+    
+    
+    
+    //step 2. Turn play (do-while)
+    //2-1. Print status
+    
+    //2-2. Roll die
+    
+    //2-3. Move (print result)
+    
+    //2-4. Change turn & Move shark
+    
+    //step 3. Game end (print winner)
+    
+    
+    
+    
+    
     // ending
-    printf("\n\n\n\n\n");
+    printf("\n\n\n\n\n\n\n");
     printf("==================================================\n");
     printf("                  SHARK GAME END                  \n");
     printf("==================================================\n");
