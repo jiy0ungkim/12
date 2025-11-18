639cc4f290b769321df12a7e7a091c02121d5efa
jiy0ungkim <2626kjy@naver.com>
2025-11-18 15:22:58 +0900

    01

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+
+int main(int argc, char *argv[])
+{
+    srand((unsigned)time(NULL));
+    
+    // opening
+    printf("==================================================\n");
+    printf("                 SHARK GAME START                 \n");
+    printf("==================================================\n");
+    
+    // ending
+    printf("\n\n\n\n\n");
+    printf("==================================================\n");
+    printf("                  SHARK GAME END                  \n");
+    printf("==================================================\n");
+    
+    system("PAUSE");	
+    return 0;
+}
